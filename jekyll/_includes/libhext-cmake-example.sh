4c1834df7947d22bbc5c719e7b6943d155bdfb4e
Tom <git@thomastrapp.com>
2018-10-19 16:30:41 +0200

    update 2018
    * update build instructions
    * add debian package install instructions
    * delete obsolete examples

--- jekyll/_includes/libhext-cmake-example.sh
+++ jekyll/_includes/libhext-cmake-example.sh
@@ -1,5 +1,5 @@
-<div class='ace-clouds'><div class='ace_static_highlight' style='counter-reset:ace_line 0'><div class='ace_line'><span class='ace_comment'># Enable C++14</span>
-</div><div class='ace_line'><span class='ace_identifier'>SET</span><span class='ace_paren ace_lparen'>(</span><span class='ace_identifier'>CMAKE_CXX_STANDARD</span> <span class='ace_constant ace_numeric'>14</span><span class='ace_paren ace_rparen'>)</span>
+<div class='ace-clouds'><div class='ace_static_highlight' style='counter-reset:ace_line 0'><div class='ace_line'><span class='ace_comment'># Enable C++17</span>
+</div><div class='ace_line'><span class='ace_identifier'>SET</span><span class='ace_paren ace_lparen'>(</span><span class='ace_identifier'>CMAKE_CXX_STANDARD</span> <span class='ace_constant ace_numeric'>17</span><span class='ace_paren ace_rparen'>)</span>
 </div><div class='ace_line'><span class='ace_comment'># Load HextConfig.cmake</span>
 </div><div class='ace_line'><span class='ace_identifier'>FIND_PACKAGE</span><span class='ace_paren ace_lparen'>(</span><span class='ace_identifier'>Hext</span> <span class='ace_identifier'>REQUIRED</span><span class='ace_paren ace_rparen'>)</span>
 </div><div class='ace_line'><span class='ace_comment'># Add libhext's include directory</span>
